7379be3d347e52c885b7aa690a097ddd3472bd37
Abdallah Bari <abdellah01win@gmail.com>
2023-11-26 12:10:41 +0100

    Code cleanup & initial testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,28 @@
 <script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+
 defineProps<{
   msg: string
 }>()
+
+const countStore = useCounterStore();
+const { count, doubleCount } = storeToRefs(countStore);
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div class="text-2xl text-blue-600">
+    <h1 class="text-red-900">{{ msg }}</h1>
+  </div>
+
+  <div class="flex gap-12">
+    <h1 class="text-3xl">{{ count }}</h1>
+    <h1 class="text-3xl">{{ doubleCount }}</h1>
+  </div>
+
+  <div>
+    <input type="number" name="count" id="count" v-model="count">
+    <button @click="countStore.increment">Increment</button>
+    <button @click="countStore.double">Double</button>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,11 +2,16 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+  const count = ref<number>(0)
+  const doubleCount = computed<number>(() => count.value * 2)
+
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  function double() {
+    count.value = count.value * 2
+  }
+
+  return { count, doubleCount, increment, double }
 })
